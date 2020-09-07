84ca89d5a773c9bd9a8b31fd3f94934309ab3e07
JuanLGC <juangcardinale@gmail.com>
2020-09-07 10:13:54 +0000

    Password encrypted, I believe

--- src/models.py
+++ src/models.py
@@ -1,9 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import cast, select, String, Text
+from sqlalchemy_utils import PasswordType
+
+
+
 
 # from sqlalchemy.ext.declarative import declarative_base
 
 # Base = declarative_base()
+
+
 
 db = SQLAlchemy()
 
@@ -18,7 +24,12 @@ association_table = db.Table('association', db.Model.metadata,
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    # password = db.Column(
+    #     PasswordType(schemes=['pbkdf2_sha512']),
+    #     unique=False,
+    #     nullable=False,
+    # )
+    password = db.Column(db.Integer)
     fullname = db.Column(db.String(50), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     gender = db.Column(db.String(30), unique=False, nullable=True)
