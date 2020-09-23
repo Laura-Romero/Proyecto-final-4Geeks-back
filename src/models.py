ce02c4656b705172692d1a3fd0ea1c821936e08d
JuanLGC <juangcardinale@gmail.com>
2020-09-23 18:49:46 +0000

    hj

--- src/models.py
+++ src/models.py
@@ -17,7 +17,6 @@ association_table = db.Table('association', db.Model.metadata,
 class User(db.Model,  UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=True)
     password = db.Column(db.String(250), unique=False, nullable=True)
     fullname = db.Column(db.String(50), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
